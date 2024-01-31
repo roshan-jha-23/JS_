fd3fc7ea9fc4aee6d3f32beee1bb6c3b6e20e068
roshan2345 <jha062305@gmail.com>
2024-01-31 23:59:58 +0530

    comparision

---
+++ comparison.js
@@ -0,0 +1,23 @@
+    //basics
+    console.log(2>3);
+    console.log(2>=3);
+    console.log(2<3);
+    console.log(2!=3);
+    console.log(2==3);
+
+
+console.log("2">1);//true bcoz "2" 2 mein badal deta hai 
+console.log("02">1);//true
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined==0);
+console.log(undefined>0);
+
+
+
+//===strict check
+console.log("2"==2);
+console.log("2"===2);//yaha no aur saath mein uske type.
