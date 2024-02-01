b3555c53024f78dfecaa60f367dff8609b55b8b1
roshan2345 <jha062305@gmail.com>
2024-02-02 01:48:07 +0530

    another array

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,10 @@
+const marvel_heros = ["thor", "ironman", "spiderman"];
+const dc_heros = ["superman", "batman", "wonderwoman"];
+
+// marvel_heros.push(dc_heros);//ararys ke andar arrays aagaya.
+// //and it proves arrays kuch bhi le leta hai jaise pura array le lita.
+
+// console.log(marvel_heros[3][1])
+
+marvel_heros.concat(dc_heros);
+console.log("Marvel Heroes: ", marvel_heros);
