27b6c25b13109ad6582a4cd86b79aa1e73266f4a
roshan2345 <jha062305@gmail.com>
2024-02-02 22:13:46 +0530

    update in 02_array file

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -7,4 +7,26 @@ const dc_heros = ["superman", "batman", "wonderwoman"];
 // console.log(marvel_heros[3][1])
 
 marvel_heros.concat(dc_heros);
-console.log("Marvel Heroes: ", marvel_heros);
+console.log("Marvel Heroes: ", marvel_heros);//it will not change original array
+console.log(marvel_heros.concat(dc_heros));
+
+const all_my_heros=[...dc_heros,...marvel_heros];//isko bolte hai spread operator kaanch ki tarah jab girta hai waise hi saare elemet bikhar gaye.
+console.log("All My Heros : ",all_my_heros)
+
+const another_array=[1,2,3,4,[5,6,7],8,[6,7,[4,5]]]
+const  flattened_arr=another_array.flat(Infinity);//Flattened Array :  [
+//   1, 2, 3, 4, 5,
+//   6, 7, 8, 6, 7,
+//   4, 5
+// ]
+console.log("Flattened Array : ",flattened_arr)
+
+
+console.log(Array.isArray("Roshan"))
+console.log(Array.from("Roshan"))
+console.log(Array.from({name:"Roshan"}))//interesting
+
+let score1=100;
+let score2=120;
+let score3=130;
+console.log(Array.of(score1,score2,score3));
