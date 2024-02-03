eebfbdb338557287908fd183992a6b08e99c5721
roshan2345 <jha062305@gmail.com>
2024-02-04 02:27:41 +0530

    call stack and how js run

---
+++ 03_basic/Callstack.js
@@ -0,0 +1,37 @@
+// function one(){
+//     console.log("one")
+// }
+// function two(){
+//     console.log("two")
+// }
+// function three(){
+//     console.log("three")
+// }
+// one()
+// two()
+// three()
+// output: one;
+// two;
+// three;
+// function one(){
+//     console.log("one")
+//     two();
+// }
+// function two(){
+//     console.log("two")
+//     three()
+// }
+// function three(){
+//     console.log("three")
+// }
+// one()
+// two()
+// three()
+// output: one;
+// two;
+// three;
+// two;
+// three;
+// three;
+// it follows basically lifo prop mt order, so it will be 1->2->3->1->2->3->1->2->3
+
