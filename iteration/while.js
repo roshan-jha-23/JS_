4f082a94df3506db2fb7646a98a7d172c8854a21
roshan2345 <jha062305@gmail.com>
2024-02-05 19:21:58 +0530

    while loop and do while

---
+++ iteration/while.js
@@ -0,0 +1,21 @@
+let i=0;
+while (true) {
+    if(i==100)break;
+   console.log(`${i}i will fucking get what i want`);
+   i++;
+}
+let myArr=["superman","batman","aquaman","arrow","flash","wondarwoman","dr doom"]
+let arr=0;
+while(arr<myArr.length){
+    console.log(`the hero ${myArr[arr]} is the best`)
+    arr++
+}
+// let score=1;
+// do {
+//     console.log("hey");
+//     score++;
+// } while (score<=10);
+let score=11;
+do {
+    console.log("hey");
+} while (score<=10);
