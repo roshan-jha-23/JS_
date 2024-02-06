d2b663c4a48f8e2411acf44e39c24da351f21dac
roshan2345 <jha062305@gmail.com>
2024-02-07 00:07:07 +0530

    proj1 background color changer button

---
+++ projects/proj1/chaiaurcode.js
@@ -0,0 +1,27 @@
+const buttons=document.querySelectorAll('.button');
+
+const body=document.querySelector( 'body' );
+
+
+
+buttons.forEach((btn)=>{
+   console.log(btn);
+   btn.addEventListener('click',(event)=>{
+      console.log(event);
+      if(event.target==='grey'){
+        body.style.backgroundColor=e.target.value;
+      }
+      if(event.target==='white'){
+        body.style.backgroundColor=e.target.value;
+      }
+      if(event.target==='blue'){
+        body.style.backgroundColor=e.target.value;
+      }
+      if(event.target==='yellow'){
+        body.style.backgroundColor=e.target.value;
+      }
+      if(event.target==='purple'){
+        body.style.backgroundColor=e.target.value;
+      }
+   })
+})
